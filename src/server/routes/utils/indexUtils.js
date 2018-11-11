910a4199964921f3e216d0cca5c1808febaea460
Noob grammar <noobgrammerofficial@gmail.com>
2018-11-11 20:03:51 +0530

    handle bug where sometimes title tokens didn't get indexed

--- src/server/routes/utils/indexUtils.js
+++ src/server/routes/utils/indexUtils.js
@@ -11,16 +11,15 @@ let indexPath = `${rootPath}/index`;
 const index = async ({title, content, uid}) => {
   const titleTokens = extractTokens(title);
   const contentTokens = extractTokens(content);
-  handleTokens(titleTokens, 'title', uid);
-  handleTokens(contentTokens, 'content', uid);
+  await handleTokens(titleTokens, 'title', uid);
+  await handleTokens(contentTokens, 'content', uid);
 }
 
 const handleTokens = async (tokens, type, uid) => {
-  const numOfTokens = tokens.length;
-  let numOfRetries = 0;
-  for (let _i = 0; _i < numOfTokens && numOfRetries < 10;) {
+  let _i = 0;
+  for (const term of tokens) {
     // convert each token to lower case
-    const token = tokens[_i].toLowerCase();
+    const token = term.toLowerCase();
     // ignore the token if the length is less than 2
     // create a file for a token if it doesnt exist and store occurences in title and content
     if (!fileExists(`${token}.json`, indexPath)) {
@@ -29,19 +28,14 @@ const handleTokens = async (tokens, type, uid) => {
         content: {}
       };
       tokenMap[type][uid] = [_i];
-      const createFileResp = await createFile(
+      const response = await createFile(
         `${token}.json`,
         indexPath,
         JSON.stringify(tokenMap),
         true
       );
-      if (createFileResp.error) {
-        console.error(createFileResp.error);
-        numOfRetries++;
-        continue;
-      } else {
-        numOfRetries = 0;
-        _i++;
+      if (response.error) {
+        console.error("Could not create file");
       }
     } else {
       const tokenMap = require(`${indexPath}/${token}.json`);
@@ -50,24 +44,18 @@ const handleTokens = async (tokens, type, uid) => {
       } else {
         tokenMap[type][uid] = [_i];
       }
-      const createFileResp = await (createFile(`${token}.json`, indexPath, JSON.stringify(tokenMap)))
-      if (createFileResp.error) {
-        console.error(createFileResp.error);
-        numOfRetries++;
-        continue;
-      } else {
-        numOfRetries = 0;
-        _i++;
+      const response = await (createFile(`${token}.json`, indexPath, JSON.stringify(tokenMap)));
+      if (response.error) {
+        console.error('Could not write to file');
       }
     }
+    _i++;
   }
 }
 
 const extractTokens = (data) => {
   const wordArray = data.replace(/[^a-zA-Z0-9]/g, ' ').split(" ").filter((w) => Boolean(w));
-  return wordArray
+  return wordArray;
 }
 
-module.exports = {
-  index,
-};
+module.exports = {index, };
